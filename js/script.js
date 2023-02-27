1ddf57ab17c36a2eff3ebcf8e3557d23f030f222
Hellonmoon <125388602+Hellonmoon@users.noreply.github.com>
2023-02-27 22:25:46 +0300

    added adaptive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopUp = document.querySelector('.login-popup');
+let closeButton = document.querySelector('.close-popup')
 
 loginButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', close);
+
 
 function showPopup(evt) {
     console.log(evt);
@@ -9,3 +12,13 @@ function showPopup(evt) {
 
     loginPopUp.classList.toggle('show-popup');
 }
+
+function close() {
+    loginPopUp.classList.remove('show-popup');
+}
+
+loginPopUp.addEventListener('click', (e) => {
+    if (evt.target.classList.contains('show-popup')) {
+        close();
+    }
+})
